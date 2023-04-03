aec7575920cce687be1b2c9b5168a58b46638dc7
shalu6_ <shalloo.k.bajpai@apisero.com>
2023-04-03 15:02:19 +0530

    written first program

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+
+
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+                Scanner sc= new Scanner(System.in);
+                int n1= sc.nextInt();
+                int n2= sc.nextInt();
+                int n3=sc.nextInt();
+                int n4=sc.nextInt();
+                int n5=sc.nextInt();
+                int total = (n1+n2+n3+n4+n5)/5;
+                System.out.println("The total percentage you obtained is :" +total);
+
+            }
+        }
